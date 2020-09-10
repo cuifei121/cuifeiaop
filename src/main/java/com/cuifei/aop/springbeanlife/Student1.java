b57c414631c5954ea1c468a81fc41ab7328189b6
cuifei <406512017@qq.com>
2020-09-10 17:00:50 +0800

    新增加的spring生命周期

---
+++ src/main/java/com/cuifei/aop/springbeanlife/Student1.java
@@ -0,0 +1,81 @@
+package com.cuifei.aop.springbeanlife;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+
+public class Student1 implements BeanFactoryAware, BeanNameAware,
+        InitializingBean, DisposableBean {
+
+    private String id;
+
+    private String name;
+
+
+    private BeanFactory beanFactory;
+    private String beanName;
+
+
+    public Student1(){
+
+        System.out.println("student 构造方法执行");
+
+    }
+
+
+
+    public void init(){
+
+        System.out.println("student inti方法执行");
+    }
+
+
+    public void studentDestroy(){
+
+        System.out.println("student destroy方法执行");
+    }
+
+
+
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("设置BeanFactory。。。。。。。。。。。。。。");
+        this.beanFactory=beanFactory;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("设置beanName。。。。。。。。。。。。。。");
+        this.beanName = name;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+        System.out.println("DisposableBean。。。。。。。。。。。。的destroy 方法进行调用");
+
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean。。。。。。。。。。。。的afterPropertiesSet 方法进行调用");
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
